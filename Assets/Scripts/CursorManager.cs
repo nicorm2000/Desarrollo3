80101ae806da8974857fb7b719b657d79690a7c3
nicorm <nicorm2000@gmail.com>
2023-08-18 11:08:17 -0300

    Created new custom Cursor system, where i can change the cursor based on an specified area, made it a singleton

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -1,23 +1,82 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CursorManager : MonoBehaviour
 {
-    [SerializeField] private List<Texture2D> cursortexture;
+    public static CursorManager Instance { get; private set; }
 
-    private Vector2 _cursorHotSpot;
+    [SerializeField] private List<CursorAnimation> cursorAnimationList;
+    
+    private CursorAnimation _cursorAnimation;
+    private int _currentFrame;
+    private int _frameCount;
+    private float _frameTimer;
 
-    public void SetNewCursor(int index)
+    public enum CursorType
     {
-        if (index >= 0 && index < cursortexture.Count)
-        {
-            _cursorHotSpot = new Vector2(cursortexture[index].width / 2, cursortexture[index].height / 2);
+        RedDot,
+        Cross
+    }
 
-            Cursor.SetCursor(cursortexture[index], _cursorHotSpot, CursorMode.Auto);
-        }
-        else
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        SetActiveCursorType(CursorType.RedDot);
+    }
+
+    private void Update()
+    {
+        _frameTimer -= Time.deltaTime;
+
+        if (_frameTimer <= 0 )
         {
-            Debug.LogError("Invalid cursor index!");
+            _frameTimer += _cursorAnimation.frameRate;
+
+            _currentFrame = (_currentFrame + 1) % _frameCount;
+
+            Cursor.SetCursor(_cursorAnimation.textureArray[_currentFrame], _cursorAnimation.offset, CursorMode.Auto);
         }
+    }
+
+    public void SetActiveCursorType(CursorType cursorType)
+    {
+        SetActiveCursorAnimation(GetCursorAnimation(cursorType));
+    }
+
+    private CursorAnimation GetCursorAnimation(CursorType cursorType)
+    {
+        foreach (CursorAnimation cursorAnimation in cursorAnimationList)
+        {
+            if (cursorAnimation.cursorType == cursorType)
+                return cursorAnimation;
+        }
+
+        // Couldn't find this cursor type
+        return null;
+    }
+
+    private void SetActiveCursorAnimation(CursorAnimation cursorAnimation)
+    {
+        this._cursorAnimation = cursorAnimation;
+
+        _currentFrame = 0;
+
+        _frameTimer = _cursorAnimation.frameRate;
+
+        _frameCount = _cursorAnimation.textureArray.Length;
+    }
+
+    [Serializable]
+    public class CursorAnimation
+    { 
+        public CursorType cursorType;
+        public Texture2D[] textureArray;
+        public float frameRate;
+        public Vector2 offset;
     }
 }
---
+++ Assets/Scripts/CursorObject.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CursorObject : MonoBehaviour
+{
+    [SerializeField] private CursorManager.CursorType cursorType;
+
+    private void OnMouseEnter()
+    {
+        CursorManager.Instance.SetActiveCursorType(cursorType);
+    }
+
+    private void OnMouseExit()
+    {
+        CursorManager.Instance.SetActiveCursorType(CursorManager.CursorType.RedDot);
+    }
+}
