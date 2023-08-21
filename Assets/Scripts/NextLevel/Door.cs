48f252e98c128692ffa13742f4d9ae30191e5ae0
fekox <facusantos123@gmail.com>
2023-08-21 18:57:16 -0300

    Fix scene change system

--- Assets/Scripts/NextLevel/Door.cs
+++ Assets/Scripts/NextLevel/Door.cs
@@ -14,13 +14,13 @@ public class Door : MonoBehaviour
         door.SetActive(false);
     }
 
-    private void OnCollisionEnter2D(Collision2D player)
+    private void OnTriggerEnter2D(Collider2D player)
     {
-        if (player.gameObject.CompareTag("Player")) 
+        if (player.gameObject.CompareTag("Player"))
         {
             Debug.Log("Colision");
             nextLevel.LoadLevel();
-        }        
+        }
     }
 
     public void ActiveObject()
