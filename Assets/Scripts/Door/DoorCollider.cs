0f59c8b2be59dbfb07fed19fcb4b475364e3773c
fekox <facusantos123@gmail.com>
2023-08-21 22:03:25 -0300

    Add a transition in the NormalLevel scene

--- Assets/Scripts/Door/DoorCollider.cs
+++ Assets/Scripts/Door/DoorCollider.cs
@@ -7,11 +7,34 @@ public class DoorCollider : MonoBehaviour
 {
     [SerializeField] private NextLevel nextLevel;
 
+    [SerializeField] private GameObject transition;
+
+    private float currentTime = 0.0f;
+    private float maxTime = 1f;
+
+    private bool starTime = false;
+
+    private void Update()
+    {
+        if (starTime == true) 
+        {
+            currentTime += Time.deltaTime;
+            Debug.Log(currentTime);
+        }
+
+        if (currentTime >= maxTime)
+        {
+            nextLevel.LoadLevel();
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D player)
     {
         if (player.gameObject.CompareTag("Player"))
         {
-            nextLevel.LoadLevel();
+            transition.SetActive(true);
+
+            starTime = true;
         }
     }
 }
