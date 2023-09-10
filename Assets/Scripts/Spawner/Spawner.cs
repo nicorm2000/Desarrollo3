26a3499cfeb65177655ff05ab89a634651856048
nicorm <nicorm2000@gmail.com>
2023-09-10 16:51:37 -0300

    Created an scalable spawn system

---
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToSpawn;
+    [SerializeField] private int amountToSpawn;
+    [SerializeField] private float spawnInterval;
+    [SerializeField] private float spawnTime;
+    [SerializeField] private Transform[] spawnPositions;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnObjects());
+    }
+
+    private IEnumerator SpawnObjects()
+    {
+        Debug.Log("Spawning!");
+        yield return new WaitForSeconds(spawnTime);
+        Debug.Log("Spawn time reached!");
+
+        for (int i = 0; i < amountToSpawn; i++)
+        {
+            GameObject spawnedObject = Instantiate(objectToSpawn, GetRandomSpawnPosition(), Quaternion.identity);
+            //Here will be the animation for the object to spawn
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    private Vector3 GetRandomSpawnPosition()
+    {
+        if (spawnPositions.Length > 0)
+        {
+            int randomIndex = Random.Range(0, spawnPositions.Length);
+            return spawnPositions[randomIndex].position;
+        }
+        else
+        {
+            return transform.position;
+        }
+    }
+}
