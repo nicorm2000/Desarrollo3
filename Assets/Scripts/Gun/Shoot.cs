fe480f2ed87088ad28ce93a1a47b1c251c305ac6
nicorm <nicorm2000@gmail.com>
2023-09-11 19:11:05 -0300

    Player can shoot full auto, started adding some assets

--- Assets/Scripts/Gun/Shoot.cs
+++ Assets/Scripts/Gun/Shoot.cs
@@ -3,12 +3,29 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
+    [SerializeField] private float attackSpeed = 10f; // Shots per second
+
+    private float timeBetweenShots;
+    private bool canShoot = true;
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0) && canShoot)
         {
-            Instantiate(bullet, transform.position, transform.rotation);
+            ShootBullet();
         }
+    }
+
+    private void ShootBullet()
+    {
+        Instantiate(bullet, transform.position, transform.rotation);
+        canShoot = false;
+        timeBetweenShots = 1f / attackSpeed;
+        Invoke(nameof(EnableShooting), timeBetweenShots);
+    }
+
+    private void EnableShooting()
+    {
+        canShoot = true;
     }
 }
