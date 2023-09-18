59fcd18e7aec25f932bb62b53cdf7e15c69342f0
nicorm2000 <nicorm2000@gmail.com>
2023-09-18 11:42:33 -0300

    Camera ignores player movement in the z axis

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private Transform target; // Player's transform
+    [SerializeField] private float cameraFollowSpeed = 5f; // Player's transform
     [SerializeField] private Vector3 offset = new Vector3(0f, 0f, -10f); // Default camera offset
 
     private void FixedUpdate()
@@ -13,8 +14,8 @@ public class CameraMovement : MonoBehaviour
             return;
         }
 
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, Time.fixedDeltaTime * 5f); // Smoothly move camera
+        Vector3 desiredPosition = new Vector3(target.position.x + offset.x, target.position.y + offset.y, transform.position.z);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, Time.fixedDeltaTime * cameraFollowSpeed); // Smoothly move camera
         transform.position = smoothedPosition;
     }
 }
