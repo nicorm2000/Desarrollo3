49319dc8fcf42aa8e66324fcdd80064d60661d4e
fekox <facusantos123@gmail.com>
2023-09-30 18:11:20 -0300

    The shop now shows the weapon you can grab

--- Assets/Scripts/Weapon/SelectWeapon.cs
+++ Assets/Scripts/Weapon/SelectWeapon.cs
@@ -6,13 +6,30 @@ public class SelectWeapon : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject levelSpawn;
+    [SerializeField] private GameObject pickUpWeaponText;
 
+    private void Start()
+    {
+        pickUpWeaponText.SetActive(false);
+    }
 
     private void OnTriggerEnter2D(Collider2D player)
     {
         if (player.gameObject.CompareTag("Player"))
         {
-            player.transform.position = levelSpawn.transform.position;
+            pickUpWeaponText.SetActive(true);
+            
+            //player.transform.position = levelSpawn.transform.position;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D player)
+    {
+        if (player.gameObject.CompareTag("Player"))
+        {
+            pickUpWeaponText.SetActive(false);
+
+            //player.transform.position = levelSpawn.transform.position;
         }
     }
 }
