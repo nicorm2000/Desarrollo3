64b2b2c720834b129a7a42441e4218cd4c8b58b8
nicorm <nicorm2000@gmail.com>
2023-10-01 18:13:19 -0300

    Fixed enemy movement speed in the soy sauce

--- Assets/Scripts/SoySauce/SlipperySurface.cs
+++ Assets/Scripts/SoySauce/SlipperySurface.cs
@@ -2,30 +2,27 @@ using UnityEngine;
 
 public class SlipperySurface : MonoBehaviour
 {
-    [SerializeField] private float slipFactor = 2.0f; // How slippery the surface is (higher values mean more slip)
-    [SerializeField] private float speedMultiplier = 1.5f; // Speed multiplier when on the slippery surface
+    [SerializeField] private float speedMultiplier = 1.5f; // Speed multiplier when inside the slippery zone
+
+    private AIChase aiChase;
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the entity entering the trigger has a Rigidbody
-        Rigidbody rb = other.GetComponent<Rigidbody>();
-        
-        if (rb != null)
+        aiChase = other.GetComponent<AIChase>();
+
+        if (aiChase != null)
         {
-            // Apply speed multiplier to the entity's velocity
-            rb.velocity *= speedMultiplier;
+            aiChase.chaseSpeed *= speedMultiplier;
         }
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerExit(Collider other)
     {
-        // Apply slip factor to the entity's velocity while staying on the slippery surface
-        Rigidbody rb = other.GetComponent<Rigidbody>();
+        aiChase = other.GetComponent<AIChase>();
 
-        if (rb != null)
+        if (aiChase != null)
         {
-            // Reduce control by increasing slip
-            rb.velocity *= slipFactor;
+            aiChase.chaseSpeed /= speedMultiplier;
         }
     }
 }
