eb66aed68f2a355f87533acc3510a037867240cc
nicorm2000 <nicorm2000@gmail.com>
2023-10-02 12:49:34 -0300

    Modified the AI Chase script

--- Assets/Scripts/Enemy/AIChase.cs
+++ Assets/Scripts/Enemy/AIChase.cs
@@ -4,8 +4,7 @@ public class AIChase : MonoBehaviour
 {
     public float chaseSpeed;
     private GameObject _player;
-
-    private float _distance;
+    public float avoidanceDistance = 1.0f; // Adjust this value as needed
 
     private void Start()
     {
@@ -14,11 +13,27 @@ public class AIChase : MonoBehaviour
 
     private void Update()
     {
-        _distance = Vector2.Distance(transform.position, _player.transform.position);
+        Vector2 playerPosition = _player.transform.position;
+        Vector2 currentPosition = transform.position;
 
-        Vector2 dir = _player.transform.position - transform.position;
-        dir.Normalize();
+        // Calculate the direction towards the player
+        Vector2 dirToPlayer = (playerPosition - currentPosition).normalized;
 
-        transform.Translate(dir * chaseSpeed * Time.deltaTime);
+        // Check for collisions in front of the enemy using Raycast
+        RaycastHit2D hit = Physics2D.Raycast(currentPosition, dirToPlayer, avoidanceDistance);
+
+        Debug.DrawRay(currentPosition, dirToPlayer, Color.red);
+
+        if (hit.collider != null)
+        {
+            // If a collision is detected, move away from the obstacle
+            Vector2 avoidanceDirection = Vector2.Perpendicular(hit.normal).normalized;
+            transform.Translate(avoidanceDirection * chaseSpeed * Time.deltaTime);
+        }
+        else
+        {
+            // If no collision, move towards the player
+            transform.Translate(dirToPlayer * chaseSpeed * Time.deltaTime);
+        }
     }
 }
