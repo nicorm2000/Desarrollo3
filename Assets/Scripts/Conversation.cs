616e2107703023670940cc352b010559ad54494b
nicorm2000 <nicorm2000@gmail.com>
2023-10-08 20:25:46 -0300

    Fixed conversation script

--- Assets/Scripts/Conversation.cs
+++ Assets/Scripts/Conversation.cs
@@ -3,31 +3,46 @@ using UnityEngine;
 
 public class Conversation : MonoBehaviour
 {
-    public TMPro.TextMeshProUGUI textMeshPro;
-    public float typingSpeed = 0.1f;
+    [SerializeField] private string[] phrases = { "Hey, this is the shop!", "Please, choose your next weapon!", "Let the carnage begin!" };
+    [SerializeField] private bool animateText = true;
+    [SerializeField] private TMPro.TextMeshProUGUI textMeshPro;
+    [SerializeField] private float typingSpeed = 0.1f;
 
-    private string[] phrases = { "Hey, this is the shop!", "Please, choose your next weapon!", "Let the carnage begin!" };
     private int currentIndex = 0;
 
-    private void Start()
+    private void OnTriggerEnter(Collider collision)
     {
-        StartCoroutine(AnimateText());
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            StartCoroutine(AnimateText());
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            StopCoroutine(AnimateText());
+        }
     }
 
     private IEnumerator AnimateText()
     {
-        while (true)
+        while (animateText)
         {
             string currentPhrase = phrases[currentIndex];
+
             textMeshPro.text = "";
 
             for (int i = 0; i < currentPhrase.Length; i++)
             {
                 textMeshPro.text += currentPhrase[i];
+
                 yield return new WaitForSeconds(typingSpeed);
             }
 
             yield return new WaitForSeconds(2f);
+
             currentIndex = (currentIndex + 1) % phrases.Length;
         }
     }
