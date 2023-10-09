93d4bb1ab2cad4fec7a46b1aa5a73ce5756bfd85
fekox <facusantos123@gmail.com>
2023-10-09 10:36:04 -0300

    Fix lose menu UI

--- Assets/Scripts/Utilities/ZValueDetection.cs
+++ Assets/Scripts/Utilities/ZValueDetection.cs
@@ -4,6 +4,8 @@ public class ZValueDetection : MonoBehaviour
 {
     [SerializeField] private Transform raycastOrigin;
     [SerializeField] private LayerMask collisionLayer;
+    [SerializeField] private float maxHeight;
+
 
     private void Update()
     {
@@ -16,7 +18,7 @@ public class ZValueDetection : MonoBehaviour
 
         Debug.DrawRay(raycastOrigin.position, transform.forward, Color.red);
 
-        if (Physics.Raycast(raycastOrigin.position, transform.forward, out hit, 1f, collisionLayer))
+        if (Physics.Raycast(raycastOrigin.position, transform.forward, out hit, maxHeight, collisionLayer))
         {
             Vector3 newPos = transform.position;
             newPos.z = hit.point.z - 0.2f;
