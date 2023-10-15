dd8d0b5c2e8287dcbac62a680ec9916a12e89137
fekox <facusantos123@gmail.com>
2023-10-15 20:00:50 -0300

    Player health can now be increased

--- Assets/Scripts/Player/UpgradePlayer.cs
+++ Assets/Scripts/Player/UpgradePlayer.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class UpgradePlayer : MonoBehaviour
 {
+    [SerializeField] private HealthBar healthBar;
+
     public PlayerData playerData;
-
-    private float currentMaxHealth;
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void UpgradeHealth(float number) 
     {
-        
+        if(playerData.currentHealth == playerData.maxHealth) 
+        {
+            playerData.maxHealth += number;
+            playerData.currentHealth = playerData.maxHealth;
+            healthBar.SetMaxHealth(playerData.maxHealth);
+            healthBar.SetHealth(playerData.currentHealth);
+
+        }
+
+        else 
+        {
+            playerData.maxHealth += number;
+            healthBar.SetHealth(playerData.currentHealth);
+        }
     }
 
     public void UpgradeSpeed(float number) 
