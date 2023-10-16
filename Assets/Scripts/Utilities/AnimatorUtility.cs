c0810d7e3b34ab8d0c2272c511c179f0b20f6bd2
nicorm2000 <nicorm2000@gmail.com>
2023-10-16 01:22:39 -0300

    Created custom animator

---
+++ Assets/Scripts/Utilities/AnimatorUtility.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using UnityEngine;
+
+public class AnimatorUtility : MonoBehaviour
+{
+    [System.Serializable]
+    public class AnimationData
+    {
+        public string animationName;
+        public Sprite[] frames;
+        public bool loop;
+        public float speed;
+    }
+
+    public AnimationData[] animations;
+
+    private SpriteRenderer spriteRenderer;
+
+    private string walk = "Walk";
+    private string idle = "Idle";
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            PlayAnimation(walk);
+            Debug.Log("Walk Animation");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            PlayAnimation(idle);
+            Debug.Log("Idle Animation");
+        }
+    }
+
+    public void PlayAnimation(string animationName)
+    {
+        AnimationData animation = FindAnimation(animationName);
+        if (animation != null)
+        {
+            StopAllCoroutines();
+            StartCoroutine(AnimateFrames(animation.frames, animation.loop, animation.speed));
+        }
+        else
+        {
+            Debug.LogError("Animation not found: " + animationName);
+        }
+    }
+
+    private AnimationData FindAnimation(string animationName)
+    {
+        foreach (AnimationData animation in animations)
+        {
+            if (animation.animationName == animationName)
+            {
+                return animation;
+            }
+        }
+        return null;
+    }
+
+    private IEnumerator AnimateFrames(Sprite[] frames, bool loop, float speed)
+    {
+        int frameIndex = 0;
+        float delay = 0.1f / speed;
+
+        while (true)
+        {
+            spriteRenderer.sprite = frames[frameIndex];
+            yield return new WaitForSeconds(delay);
+
+            frameIndex++;
+            if (frameIndex >= frames.Length)
+            {
+                if (loop)
+                {
+                    frameIndex = 0;
+                    if (frames[frames.Length - 1])
+                    {
+                        Debug.Log("Animation finished: " + frames[frames.Length - 1]);
+                        //Destroy(gameObject);
+                    }
+                }
+                else
+                {
+                    Debug.Log("Animation finished: " + frames[frames.Length - 1]);
+                    break;
+                }
+            }
+        }
+    }
+}
