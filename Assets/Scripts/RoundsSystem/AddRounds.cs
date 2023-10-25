8f1b93bae480c8e559d27729ca4116fd5b06e368
nicorm2000 <nicorm2000@gmail.com>
2023-10-24 21:57:12 -0300

    Fixed slower ability not dissapearing

--- Assets/Scripts/RoundsSystem/AddRounds.cs
+++ Assets/Scripts/RoundsSystem/AddRounds.cs
@@ -7,12 +7,30 @@ public class AddRounds : MonoBehaviour
 
     public WaveData waveData;
 
+    private int targetLayer;
+
+    private void Start()
+    {
+        targetLayer = LayerMask.NameToLayer("Slower");
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
+            GameObject[] slowerObjects = GameObject.FindObjectsOfType<GameObject>();
+
             roundCounter.IncreaseRounds(waveData.addRound);
+
             shop.ActiveShop();
+
+            foreach (GameObject obj in slowerObjects)
+            {
+                if (obj.layer == targetLayer)
+                {
+                    Destroy(obj);
+                }
+            }
         }
     }
 }
