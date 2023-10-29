15e45674c00548e880db55cf2175150f3cbfef0a
nicorm2000 <nicorm2000@gmail.com>
2023-10-29 16:19:30 -0300

    Made a wave manager with 3 waves to test

--- Assets/Scripts/Spawner/WaveManager.cs
+++ Assets/Scripts/Spawner/WaveManager.cs
@@ -1,8 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    
+    private IWave[] _wave;
+    private int _currentWaveIndex = 0;
+
+    private void Start()
+    {
+        _wave = new IWave[]
+        {
+            new Wave1(),
+            new Wave2(),
+            new Wave3()
+        };
+
+        ActivateCurrentWave();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            DeactivateCurrentWave();
+            SwitchToNextWave();
+        }
+    }
+
+    private void ActivateCurrentWave()
+    {
+        _wave[_currentWaveIndex].Activate();
+    }
+
+    private void DeactivateCurrentWave()
+    {
+        _wave[_currentWaveIndex].Deactivate();
+    }
+
+    private void SwitchToNextWave()
+    {
+        if (_currentWaveIndex < _wave.Length - 1)
+        {
+            _currentWaveIndex++;
+            ActivateCurrentWave();
+        }
+        else 
+        {
+            Debug.Log("Final wave");
+            _currentWaveIndex = _wave.Length - 1;
+        }
+    }
 }
