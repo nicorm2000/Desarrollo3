ba330a67b3a69cca43a9c1ec4bc9ebd20738ea89
nicorm2000 <nicorm2000@gmail.com>
2023-11-02 17:50:04 -0300

    Fixed camera follow script to work with late update instead of fixed update

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -5,16 +5,13 @@ public class CameraMovement : MonoBehaviour
     [Header("Target to Follow")]
     [SerializeField] private Transform target;
     
-    [Header("Camera Follow Speed")]
-    [SerializeField] private float cameraFollowSpeed;
-    
     [Header("Camera Offset")]
     [SerializeField] private Vector3 offset;
 
     [Header("Player Data Dependencies")]
     [SerializeField] private PlayerData playerData;
 
-    private void FixedUpdate() //Keep the camera in FixedUpdate beacuase if not, the player looks blurry with its movement 
+    private void LateUpdate()
     {
         if (playerData.transform == null)
         {
@@ -23,7 +20,6 @@ public class CameraMovement : MonoBehaviour
         }
 
         Vector3 desiredPosition = new Vector3(target.position.x + offset.x, target.position.y + offset.y, transform.position.z);
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, Time.fixedDeltaTime * cameraFollowSpeed);
-        transform.position = smoothedPosition;
+        transform.position = desiredPosition;
     }
 }
