885d6a87fccde34f58b53307f50d931dd3848cbb
nicorm2000 <nicorm2000@gmail.com>
2023-11-03 15:02:53 -0300

    Optimized the LookAtMouse script

--- Assets/Scripts/Cursor/LookAtMouse.cs
+++ Assets/Scripts/Cursor/LookAtMouse.cs
@@ -2,36 +2,65 @@ using UnityEngine;
 
 public class LookAtMouse : MonoBehaviour
 {
-    private Camera mainCamera;
+    [SerializeField] private PlayerData playerData;
 
-    public PlayerData playerData;
-
+    /// <summary>
+    /// Sets the transform parent of the player data.
+    /// </summary>
     private void Start()
     {
-        mainCamera = Camera.main;
-        playerData.transform = transform.parent;
+        SetPlayerDataTransformParent();
     }
 
+    /// <summary>
+    /// Updates the scale and rotation of the object based on the mouse position.
+    /// </summary>
     private void Update()
     {
+        Vector3 mouseWorldPosition = GetMouseWorldPosition();
+        float rotationAngle = CalculateRotationAngle(mouseWorldPosition);
+        SetScaleAndRotation(rotationAngle);
+    }
+
+    /// <summary>
+    /// Retrieves the mouse position in the world space.
+    /// </summary>
+    /// <returns>The mouse position in the world space.</returns>
+    private Vector3 GetMouseWorldPosition()
+    {
         Vector3 mousePosition = Input.mousePosition;
-        Vector3 mouseScreenPosition = mainCamera.WorldToScreenPoint(transform.position);
+        Vector3 mouseScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
         Vector3 offsetMousePosition = new Vector3(mousePosition.x, mousePosition.y, mouseScreenPosition.z);
 
-        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(offsetMousePosition);
-        Vector3 directionToMouse = mouseWorldPosition - transform.position;
-        float rotationAngle = Mathf.Atan2(directionToMouse.y, directionToMouse.x) * Mathf.Rad2Deg;
+        return Camera.main.ScreenToWorldPoint(offsetMousePosition);
+    }
 
-        if (rotationAngle > 90 || rotationAngle < -90)
-        {
-            transform.localScale = new Vector3(1f, -1f, 1f);
-        }
-        else
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        }
-        
-        transform.rotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
-        playerData.transform.position = transform.position;
+    /// <summary>
+    /// Calculates the rotation angle based on the mouse position.
+    /// </summary>
+    /// <param name="mouseWorldPosition">The mouse position in the world space.</param>
+    /// <returns>The rotation angle.</returns>
+    private float CalculateRotationAngle(Vector3 mouseWorldPosition)
+    {
+        Vector3 directionToMouse = mouseWorldPosition - transform.position;
+        return Mathf.Atan2(directionToMouse.y, directionToMouse.x) * Mathf.Rad2Deg;
+    }
+
+    /// <summary>
+    /// Sets the scale and rotation of the object based on the rotation angle.
+    /// </summary>
+    /// <param name="rotationAngle">The rotation angle.</param>
+    private void SetScaleAndRotation(float rotationAngle)
+    {
+        transform.localScale = new Vector3(1f, rotationAngle > 90 || rotationAngle < -90 ? -1f : 1f, 1f);
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationAngle);
+    }
+
+    /// <summary>
+    /// Sets the transform parent of the player data.
+    /// </summary>
+    private void SetPlayerDataTransformParent()
+    {
+        playerData.transform = transform.parent;
     }
 }
