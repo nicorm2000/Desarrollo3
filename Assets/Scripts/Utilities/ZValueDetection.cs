4108a8fb0c08588a643218ebd8042f436bad0b38
nicorm2000 <nicorm2000@gmail.com>
2023-11-03 14:13:56 -0300

    Optimized ZValueDetection script

--- Assets/Scripts/Utilities/ZValueDetection.cs
+++ Assets/Scripts/Utilities/ZValueDetection.cs
@@ -1,45 +1,83 @@
+using System;
 using UnityEngine;
 
 public class ZValueDetection : MonoBehaviour
 {
+    [Header("Raycast's Transform")]
     [SerializeField] private Transform raycastTop;
     [SerializeField] private Transform raycastBot;
-    [SerializeField] private LayerMask collisionLayer;
-    [SerializeField] private float maxHeight = 2f;
 
+    [Header("Raycast height")]
+    [SerializeField] private float maxHeight;
+
+    [Header("Position Offset")]
+    [SerializeField] private float positionOffset;
+
+    [Header("Interacting Layers")]
+    [SerializeField] private LayerMask includeLayer;
+
+    [Header("RayCast Color")]
+    [SerializeField] private Color raycastColor;
+
+    /// <summary>
+    /// Updates the Z position of the object.
+    /// </summary>
     private void Update()
     {
         UpdateZPosition();
     }
 
+    /// <summary>
+    /// Performs raycasts to determine the Z position of the object based on the hit points.
+    /// </summary>
     private void UpdateZPosition()
     {
-        if (Physics.Raycast(raycastBot.position, transform.forward, out RaycastHit hit1, maxHeight, collisionLayer) 
-            && Physics.Raycast(raycastTop.position, transform.forward, out RaycastHit hit2, maxHeight, collisionLayer))
+        if (Physics.Raycast(raycastBot.position, transform.forward, out RaycastHit hit1, maxHeight, includeLayer) 
+            && Physics.Raycast(raycastTop.position, transform.forward, out RaycastHit hit2, maxHeight, includeLayer))
         {
-            if (hit1.transform.position.z == hit2.transform.position.z)
+            float value1 = hit1.transform.position.z;
+            float value2 = hit2.transform.position.z;
+
+            if (value1 == value2)
             {
                 Vector3 newPos = transform.position;
-                newPos.z = hit1.point.z - 0.2f;
-                transform.position = newPos;
+                UpdateZPositionBasedOnPoint(newPos, hit1);
             }
             else
             {
                 Vector3 newPos = transform.position;
-                if (hit1.transform.position.z < hit2.transform.position.z)
+                if (value1 < value2)
                 {
-                    newPos.z = hit1.point.z - 0.2f;
-                    transform.position = newPos;
+                    UpdateZPositionBasedOnPoint(newPos, hit1);
                 }
-                else if (hit1.transform.position.z > hit2.transform.position.z)
+                else if (value1 > value2)
                 {
-                    newPos.z = hit2.point.z - 0.2f;
-                    transform.position = newPos;
+                    UpdateZPositionBasedOnPoint(newPos, hit2);
                 }
             }
         }
 
-        Debug.DrawRay(raycastTop.position, transform.forward * maxHeight, Color.red);
-        Debug.DrawRay(raycastBot.position, transform.forward * maxHeight, Color.red);
+        DebugRays(raycastColor);
+    }
+
+    /// <summary>
+    /// Updates the Z position of the object based on a specific hit point.
+    /// </summary>
+    /// <param name="pos">The current position of the object.</param>
+    /// <param name="value">The RaycastHit containing the hit point information.</param>
+    private void UpdateZPositionBasedOnPoint(Vector3 pos, RaycastHit value)
+    {
+        pos.z = value.point.z - positionOffset;
+        transform.position = pos;
+    }
+
+    /// <summary>
+    /// Draws debug rays with a specified color for visualization purposes.
+    /// </summary>
+    /// <param name="rayColor">The color of the debug rays.</param>
+    private void DebugRays(Color rayColor)
+    {
+        Debug.DrawRay(raycastTop.position, transform.forward * maxHeight, rayColor);
+        Debug.DrawRay(raycastBot.position, transform.forward * maxHeight, rayColor);
     }
 }
