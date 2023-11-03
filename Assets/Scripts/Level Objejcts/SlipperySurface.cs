57b8378922c1442cca69274c5a1a43d1c8124d61
nicorm2000 <nicorm2000@gmail.com>
2023-11-03 13:01:09 -0300

    Updated slipperysurface to check for the layer of collision and then do its logic

--- Assets/Scripts/Level Objejcts/SlipperySurface.cs
+++ Assets/Scripts/Level Objejcts/SlipperySurface.cs
@@ -2,38 +2,56 @@ using UnityEngine;
 
 public class SlipperySurface : MonoBehaviour
 {
+    [Header("Interacting Layers")]
+    [SerializeField] private LayerMask includeLayer;
+
+    [Header("Speed Multiplier")]
     [SerializeField] private float speedMultiplier = 1.5f;
 
     private AIChase aiChase;
     private AIShooterChase aiShooterChase;
 
+    /// <summary>
+    /// Handles the event when a collider enters the trigger, increasing the chase speed of the AI if its layer is included.
+    /// </summary>
+    /// <param name="other">The collider that entered the trigger.</param>
     private void OnTriggerEnter(Collider other)
     {
-        aiChase = other.GetComponent<AIChase>();
-        aiShooterChase = other.GetComponent<AIShooterChase>();
+        if (((Constants.ONE << other.gameObject.layer) & includeLayer) != Constants.ZERO)
+        {
+            aiChase = other.GetComponent<AIChase>();
+            aiShooterChase = other.GetComponent<AIShooterChase>();
 
-        if (aiChase != null)
-        {
-            aiChase.chaseSpeed *= speedMultiplier;
-        }
-        else if (aiShooterChase != null)
-        {
-            aiShooterChase.chaseSpeed *= speedMultiplier;
+            if (aiChase != null)
+            {
+                aiChase.chaseSpeed *= speedMultiplier;
+            }
+            else if (aiShooterChase != null)
+            {
+                aiShooterChase.chaseSpeed *= speedMultiplier;
+            }
         }
     }
 
+    /// <summary>
+    /// Handles the event when a collider exits the trigger, decreasing the chase speed of the AI if its layer is included.
+    /// </summary>
+    /// <param name="other">The collider that exited the trigger.</param>
     private void OnTriggerExit(Collider other)
     {
-        aiChase = other.GetComponent<AIChase>();
-        aiShooterChase = other.GetComponent<AIShooterChase>();
+        if (((Constants.ONE << other.gameObject.layer) & includeLayer) != Constants.ZERO)
+        {
+            aiChase = other.GetComponent<AIChase>();
+            aiShooterChase = other.GetComponent<AIShooterChase>();
 
-        if (aiChase != null)
-        {
-            aiChase.chaseSpeed /= speedMultiplier;
-        }
-        else if (aiShooterChase != null)
-        {
-            aiShooterChase.chaseSpeed /= speedMultiplier;
+            if (aiChase != null)
+            {
+                aiChase.chaseSpeed /= speedMultiplier;
+            }
+            else if (aiShooterChase != null)
+            {
+                aiShooterChase.chaseSpeed /= speedMultiplier;
+            }
         }
     }
 }
