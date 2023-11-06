309a8eeeade282e77b01e55bedaa7e3035fba272
nicorm2000 <nicorm2000@gmail.com>
2023-11-06 20:32:51 -0300

    Finished camera drag mechanic

--- Assets/Scripts/Camera/CameraDrag.cs
+++ Assets/Scripts/Camera/CameraDrag.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class CameraDrag : MonoBehaviour
@@ -5,14 +6,20 @@ public class CameraDrag : MonoBehaviour
     [Header("Drag Configuration")]
     [SerializeField] private Transform targetTransform;
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float returnSpeedMultiplier;
+    [SerializeField] private float returnSpeedTime;
+    [SerializeField] private AnimationCurve returnAnimationCurve;
 
     private Vector3 initialPosition;
+    private Vector3 releasePosition;
+    private float timer = 0f;
     private bool isDragging = false;
+
+    private Coroutine coroutine = null;
 
     private void Start()
     {
         initialPosition = transform.position;
+        releasePosition = initialPosition;
     }
 
     private void Update()
@@ -25,22 +32,44 @@ public class CameraDrag : MonoBehaviour
         if (Input.GetMouseButtonUp(1))
         {
             isDragging = false;
+            releasePosition = transform.position;
+
+            if (coroutine == null)
+            {
+                coroutine = StartCoroutine(ReturnToOriginalPosition());
+            }
         }
 
         if (isDragging)
         {
             float mouseX = Input.GetAxis("Mouse X");
             float mouseY = Input.GetAxis("Mouse Y");
-
             Vector3 movement = new Vector3(-mouseX, -mouseY, 0f) * moveSpeed * Time.deltaTime;
             transform.Translate(movement, Space.World);
         }
-        else
+    }
+
+    private IEnumerator ReturnToOriginalPosition()
+    {
+        timer = 0;
+
+        while (timer < returnSpeedTime)
         {
-            transform.position = Vector3.Lerp(transform.position, targetTransform.position, moveSpeed * Time.deltaTime);
-            float distance = Vector3.Distance(transform.position, initialPosition);
-            float returnSpeed = moveSpeed * returnSpeedMultiplier / distance;
-            transform.position = Vector3.Lerp(transform.position, initialPosition, Mathf.Clamp01(returnSpeed * Time.deltaTime));
+            float progress = timer / returnSpeedTime;
+            float curveValue = returnAnimationCurve.Evaluate(progress);
+            transform.position = Vector3.Lerp(releasePosition, initialPosition, curveValue);
+            timer += Time.deltaTime;
+
+            if (isDragging)
+            {
+                transform.position = initialPosition;
+                yield return null;
+            }
+
+            yield return null;
         }
+
+        transform.position = initialPosition;
+        coroutine = null;
     }
 }
