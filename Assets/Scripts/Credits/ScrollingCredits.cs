059c8ba6715423b78db67c7486326868e8dbc8fd
nicorm2000 <nicorm2000@gmail.com>
2023-11-07 22:16:51 -0300

    Finished credits script that scroll based on animation curve

---
+++ Assets/Scripts/Credits/ScrollingCredits.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class ScrollingCredits : MonoBehaviour
+{
+    [Header("Scroll Configuration")]
+    [SerializeField] private float scrollTime;
+    [SerializeField] private AnimationCurve scrollSpeedCurve;
+    [SerializeField] private Vector3 initialPosition;
+    [SerializeField] private Vector3 targetPosition;
+
+    [Header("Main Menu Button")]
+    [SerializeField] private GameObject mainMenuButton;
+
+    [Header("Objects Appearing")]
+    [SerializeField] private float object1Time;
+    [SerializeField] private float object2Time;
+    [SerializeField] private GameObject object1;
+    [SerializeField] private GameObject object2;
+
+    private void Start()
+    {
+        StartCoroutine(ScrollCredits());
+    }
+
+    private IEnumerator ScrollCredits()
+    {
+        float timer = 0f;
+
+        while (timer < scrollTime)
+        {
+            float progress = timer / scrollTime;
+            float scrollSpeed = scrollSpeedCurve.Evaluate(progress);
+
+            transform.position = Vector3.Lerp(initialPosition, targetPosition, scrollSpeed);
+
+            timer += Time.deltaTime;
+
+            if (progress >= object1Time)
+            {
+                object1.SetActive(true);
+            }
+
+            if (progress >= object2Time)
+            {
+                object2.SetActive(true);
+            }
+
+
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        mainMenuButton.SetActive(true);
+    }
+}
