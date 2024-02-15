10ecb6b2590476f1b18d39921b4fe861f2641506
fekox <facusantos123@gmail.com>
2024-02-15 16:18:47 -0300

    Add BossAnimationStateController script

---
+++ Assets/Scripts/Boss/BossAnimationStateController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAnimationStateController : MonoBehaviour
+{
+    [Header("Setup")]
+    [SerializeField] private Animator animator;
+
+    private void OnEnable()
+    {
+
+    }
+    private void OnDisable()
+    {
+
+    }
+
+    private void HandleBossIdleChange(bool isIdle)
+    {
+
+    }
+
+    private void HandleBossFirstAttackChange(bool isFirstAttack)
+    {
+
+    }
+
+    private void HandleBossSecondAttackChange(bool isSecondAttack)
+    {
+
+    }
+
+    private void HandleBossThirdAttackChange(bool isThirdAttack) 
+    {
+    
+    }
+
+    private void HandleBossDeadChange(bool isDead) 
+    {
+    
+    }
+}
