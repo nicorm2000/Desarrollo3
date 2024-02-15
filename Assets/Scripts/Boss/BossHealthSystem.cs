a4fd822fd5fd4c661e63dfa30110a3d19f129775
fekox <facusantos123@gmail.com>
2024-02-15 16:29:39 -0300

    Add SetHealth function on BossHealthSystem script

--- Assets/Scripts/Boss/BossHealthSystem.cs
+++ Assets/Scripts/Boss/BossHealthSystem.cs
@@ -6,6 +6,11 @@ public class BossHealthSystem : MonoBehaviour
 {
     [SerializeField] private BossData bossData;
 
+    public void SetHealth(float maxHealth) 
+    {
+        bossData.health = maxHealth;
+    }
+
     public void TakeDamage(float damage) 
     {
         bossData.health -= damage;
